2a78a663e0108deb436b0c10c7dea551db0e9054
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-06-18 15:06:09 -0500

    learn vue3

---
+++ helloworld/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <DirectivaIterativa></DirectivaIterativa>
+</template>
+
+<script lang="ts" setup>
+// import ButtonComponet from './components/ButtonComponet.vue';
+// import DirectivaSelectiva from './components/DirectivaSelectiva.vue';
+import DirectivaIterativa from './components/DirectivaIterativa.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ helloworld/src/components/DirectivaSelectiva.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- uso de ridectivas v-if - v-else - v-show  -->
+    <!-- Uso de v-if es cuando no hay muchos cambios -->
+    <!-- Uso de v-show es cuando existe reactividad constante, se agrega un class:display-none en la etiqueta -->
+    <!-- <h1 v-if="date > 18">Soy mayor de edad</h1>
+    <h1 v-else-if="date = 18">Cumpli los 18 años</h1>
+    <h1 v-else>Soy menos de edad</h1> -->
+    <h1 v-show="age > 18">Soy mayor de edad</h1>
+    <h1 v-show="age < 18">Soy menor de edad</h1>
+</template>
+
+<script setup lang="ts">
+
+const age = 50;
+
+</script>
+
+<style scoped>
+
+</style>

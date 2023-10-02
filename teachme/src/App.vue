4b83f8cc500277e6a5b8f3aea0f34892f09bd6cb
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-10-02 11:53:00 -0500

    ciclo de vida props, emits

--- teachme/src/App.vue
+++ teachme/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <img alt="Vue logo" src="./assets/logo.png">
   <FormTeachme/>
 </template>
 
@@ -8,12 +8,5 @@ import FormTeachme from './components/FormTeachme.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

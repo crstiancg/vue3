c959dd863f1ab5c55996cf8cd27d3d655c69645d
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-10-24 20:23:16 -0500

    component register

--- componentsp/src/App.vue
+++ componentsp/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <AuthLogin/>
   <!-- <CardLogin/> -->
+  <AuthRegister/>
 </template>
 
 <script setup>
 import AuthLogin from './components/AuthLogin.vue'
 // import CardLogin from './components/CardLogin.vue';
+import AuthRegister from './components/AuthRegister.vue';
 </script>
 
 <style>
